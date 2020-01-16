b81de069ca350c0a2fa0c4d5f73f63d9dfb8e4c3
unknown <chihachoi@outlook.com>
2020-01-16 23:46:42 +0000

    refactor code

--- index.js
+++ index.js
@@ -7,18 +7,58 @@ var Jimp = require("jimp");
 // Create an instance of a Discord client
 const client = new Client();
 
-const list = [
-    'mchale',
-    'cryglasses',
-    'handshake',
-    'wtfamireading',
-    'jasonface',
-    'lava',
-    'burninghouse',
-    'flextape',
-    'nelson',
-    'wegetitdan'
+const memeList = [
+    {
+        name:'mchale',
+        textNames: ['mchale','booze']
+    },
+    {
+        name:'cryglasses',
+        textNames: ['top']
+    },
+    {
+        name:'handshake',
+        textNames: ['lefthand','righthand','handshake']
+    },
+    {
+        name:'wtfamireading',
+        textNames: ['top','bot']
+    },
+    {
+        name:'jasonface',
+        textNames: ['face text']
+    },
+    {
+        name:'lava',
+        textNames: ['guy','lava']
+    },
+    {
+        name:'burninghouse',
+        textNames: ['house','kid']
+    },
+    {
+        name:'flextape',
+        textNames: ['guy','tape']
+    },
+    {
+        name:'nelson',
+        textNames: ['nelson','viewers']
+    },
+    {
+        name:'wegetitdan',
+        textNames:  ['dan']
+    },
+    {
+        name: 'distracted',
+        textNames: ['spicy piece of ass','bf','gf']
+    }
 ]
+// populate list of memes for help function
+var memeNames = []
+memeList.forEach((e)=>{
+    memeNames.push(e.name)
+})
+//
 
 /**
  * The ready event is vital, it means that only after this will your bot start reacting to information
@@ -33,82 +73,18 @@ client.on('message', message => {
         const command = message.content.slice(1).split(' ')[0]
         const args = message.content.substring(message.content.indexOf(' ')+1).split(',,')
         console.log(args)
-        switch (command) {
-            case 'handshake':
-                if (args.length!=3){
-                    verify(message, 'handshake',['lefthand text','righthand text','handshake text'])
-                } else {
-                    handshake(message, args)
-                }
-                break;
-            case 'mchale':
-                if (args.length!=2){
-                    verify(message, 'mchale',['mchale text','booze text'])
-                } else {
-                    mchale(message, args)
-                }
-                break;
-            case 'cryglasses':
-                if (args.length!=1){
-                    verify(message, 'cryglasses',['top text'])
-                } else {
-                    cryglasses(message, args)
-                }
-                break;
-            case 'wtfamireading':
-                if (args.length!=2){
-                    verify(message, 'wtfamireading',['top text', 'bot text'])
-                } else {
-                    wtfamireading(message, args)
-                }
-                break;
-            case 'jasonface':
-                if (args.length!=1){
-                    verify(message, 'jasonface',['top text'])
-                } else {
-                    jasonface(message, args)
-                }
-                break;
-            case 'lava':
-                if (args.length!=2){
-                    verify(message, 'lava',['top text, lava text'])
-                } else {
-                    lava(message, args)
-                }
-                break;
-            case 'burninghouse':
-                if (args.length!=2){
-                    verify(message, 'burninghouse',['house text, kid text'])
-                } else {
-                    burninghouse(message, args)
-                }
-                break;
-            case 'flextape':
-                if (args.length!=2){
-                    verify(message, 'flextape',['guy text, tape text'])
-                } else {
-                    flextape(message, args)
-                }
-                break;
-            case 'nelson':
-                if (args.length!=2){
-                    verify(message, 'nelson',['nelson text, viewers text'])
-                } else {
-                    nelson(message, args)
-                }
-                break;
-            case 'wegetitdan':
-                if (args.length!=1){
-                    verify(message, 'wegetitdan',['wegetitdan text'])
-                } else {
-                    wegetitdan(message, args)
-                }
-                break;
-                
-            case 'help':
-                message.channel.send(`supported images are: ${list}`)
+        if (command === 'help'){
+            message.channel.send(`supported images are: ${memeNames}`);
         }
-
+        memeList.forEach((meme,i)=>{
+            if(command === meme.name){
+                if(args.length === meme.textNames.length){
+                    eval(command)(message,args)
+                } else {
+                    verify(message, meme.name, meme.textNames)
+                }
+            }
+        })
     }
 });
 
@@ -125,66 +101,85 @@ function handshake(message, args){
     const firstText = [275,500]
     const secondText = [680,390]
     const thirdText = [380,100]
-    sendImage(message, args, 'handshake.jpg', [firstText,secondText,thirdText], [500,200])
+    const textSize = [500,200]
+    sendImage(message, args, 'handshake.jpg', [firstText,secondText,thirdText], textSize)
 }
 
 function mchale(message, args){
     const firstText = [410,150]
     const secondText = [140,650]
-    sendImage(message, args, 'mchale.jpg', [firstText,secondText], [250,400])
+    const textSize = [250,400]
+    sendImage(message, args, 'mchale.jpg', [firstText,secondText], textSize)
 }
 
 function cryglasses(message, args){
     const firstText = [80,20]
-    sendImage(message, args, 'cryglasses.jpg', [firstText], [160,100], 16)
+    const textSize = [160,100]
+    sendImage(message, args, 'cryglasses.jpg', [firstText], textSize, 16)
 }
 
 function wtfamireading(message,args){
     const firstText = [177,80]
     const secondText = [200,681]
-    sendImage(message, args, 'wtfamireading.png', [firstText,secondText], [400,100])
+    const textSize = [400,100]
+    sendImage(message, args, 'wtfamireading.png', [firstText,secondText], textSize)
 }
 
 function jasonface(message,args){
     const firstText = [267,476]
-    sendImage(message, args, 'jasonface.jpg', [firstText], [400,100])
+    const textSize = [400,100]
+    sendImage(message, args, 'jasonface.jpg', [firstText], textSize)
 }
 
 function lava(message,args){
     const firstText = [380,80]
     const secondText = [380,400]
-    sendImage(message, args, 'lava.jpg', [firstText, secondText], [400,100])
+    const textSize = [400,400]
+    sendImage(message, args, 'lava.jpg', [firstText, secondText], textSize,  yAlign=Jimp.VERTICAL_ALIGN_TOP)
 }
 
 function burninghouse(message,args){
     const firstText = [130,60]
     const secondText = [230,310]
-    sendImage(message, args, 'burninghouse.jpg', [firstText, secondText], [400,100], 32)
+    const textSize = [400,100]
+    sendImage(message, args, 'burninghouse.jpg', [firstText, secondText], textSize, 32)
 }
 
 function flextape(message,args){
     const firstText = [600,500]
     const secondText = [300,1610]
-    sendImage(message, args, 'flextape.jpg', [firstText, secondText], [700,100],128)
+    const textSize = [700,100]
+    sendImage(message, args, 'flextape.jpg', [firstText, secondText], textSize,128)
 }
 
 function nelson(message,args){
     const firstText = [190,220]
     const secondText = [530,500]
-    sendImage(message, args, 'nelson.png', [firstText, secondText], [300,100])
+    const textSize = [300,100]
+    sendImage(message, args, 'nelson.png', [firstText, secondText], textSize)
 }
 
 function wegetitdan(message,args){
     const firstText = [370,670]
-    sendImage(message, args, 'wegetitdan.jpg', [firstText], [500,100])
+    const textSize = [500,100]
+    sendImage(message, args, 'wegetitdan.jpg', [firstText], textSize)
 }
+
+function distracted(message,args){
+    const firstText = [190,380]
+    const secondText = [500,380]
+    const thirdText = [700,380]
+    const textSize = [200,300]
+    sendImage(message, args, 'distracted.jpg', [firstText,secondText,thirdText], textSize, yAlign=Jimp.VERTICAL_ALIGN_TOP)
+}
+
 
 
 
 
 
 function sendImage(message, args, image, textPositions, texboxSize, fontSize){
-    Jimp.read(image)
+    Jimp.read('images/' + image)
     .then(function (image) {
       loadedImage = image;
       if (fontSize===16){
@@ -208,16 +203,18 @@ function sendImage(message, args, image, textPositions, texboxSize, fontSize){
 
 }
 
-function addText(image, text, textPosition, textBoxSize, font ){
+function addText(image, text, textPosition, textBoxSize, font, yAlign ){
+    yAlign = yAlign | Jimp.VERTICAL_ALIGN_MIDDLE
+    console.log(yAlign)
     text.forEach((textContent,i)=>{
         image.print(
             font,
-            textPosition[i][0] - textBoxSize[0]/2,
-            textPosition[i][1] - textBoxSize[1]/2,
+            textPosition[i][0] - textBoxSize[0]/2, //x coordinate
+            textPosition[i][1] - textBoxSize[1]/2, //y coordinate
             {
                 text: textContent,
                 alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-                alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+                alignmentY: yAlign
             },
             textBoxSize[0],
             textBoxSize[1]
