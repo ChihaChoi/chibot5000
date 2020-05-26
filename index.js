3307dfb4fbbe31b0d4e547d17f11834bfea905f1
chiha.choi <chiha.choi@uk.rsagroup.com>
2020-05-26 12:34:29 +0100

    remove tolowercase

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ client.on('message', message => {
                 }
             }
         })
-    } else if (message.content.toLowerCase().includes('liddard')){
+    } else if (message.content.includes('liddard')){
         message.channel.send('can we just all try to be nicer to one another as part of my new year resolution I just want less drama and more good vibes')
-    } else if (message.content.toLowerCase().includes('good bot')){
+    } else if (message.content.includes('good bot')){
         message.channel.send(':blush:')
     }
 });
